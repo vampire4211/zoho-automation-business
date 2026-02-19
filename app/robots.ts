e2adc8158034edb9dd79aa2861e04a40575229c9
vampire4211 <vampjay4211@gmail.com>
2026-02-19 22:29:41 +0530

    feat: Add `robots.ts` to configure web crawler access rules and sitemap.

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://thecleararchitect.com';
+    const baseUrl = 'https://jp-business-automation.in';
 
     return {
         rules: [
